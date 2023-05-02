0c5f9f5cd96d27004fb2ae7bef2ba001d0ac3402
Reginald Jones <regjones16@outlook.com>
2023-05-02 17:49:01 -0500

    Update code

--- backend.tf
+++ backend.tf
@@ -5,6 +5,6 @@ backend "azurerm" {
     resource_group_name  = "test-grp"
     storage_account_name = "regterraformstate201"
     container_name       = "tfstatefiles"
-    key                  = "azure-container-app-terraform.tfstate"
+    key                  = "pokedex-app-service-terraform.tfstate"
   }
 }
--- main.tf
+++ main.tf
@@ -1,11 +1,32 @@
 resource "azurerm_resource_group" "example" {
-  name     = "container-app-rg"
+  name     = "python-container-resources"
   location = "Central US"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
+resource "azurerm_container_group" "example" {
+  name                = "example-container-group"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+  ip_address_type     = "Public"
+  dns_name_label      = "example-azure-container-app"
+  os_type             = "Linux"
+
+  container {
+    name   = "example-container"
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    Terraform = "true"
+  }
 }
+
+
+
